ac9dbb419ac00ab83c4b5a194daed017c7335e5b
Sunny <55818652+sunnyliketheweather94@users.noreply.github.com>
2025-11-19 19:02:13 -0600

    created class for Markov process

--- reinforcement_learning/RL4Finance/rl/markov_process.py
+++ reinforcement_learning/RL4Finance/rl/markov_process.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Callable, Generic, Iterable, TypeVar
+from typing import Callable, Generic, Iterable, Mapping, Sequence, Set, TypeVar
 
-from rl.distributions import Distribution
+import numpy as np
+
+from rl.distributions import Categorical, Distribution, FiniteDistribution
 
 S = TypeVar("S")
 X = TypeVar("X")
@@ -44,3 +46,70 @@ class MarkovProcess(ABC, Generic[S]):
         while isinstance(state, NonTerminal):
             state = self.transition(state=state).sample()
             yield state
+
+
+Transition = Mapping[NonTerminal[S], FiniteDistribution[State[S]]]
+
+
+class FiniteMarkovProcess(MarkovProcess[S]):
+    non_terminal_states: Sequence[NonTerminal[S]]
+    transition_map: Transition[S]
+
+    def __init__(self, transition_map: Mapping[S, FiniteDistribution[S]]):
+        non_terminals: Set[S] = set(transition_map.keys())
+
+        self.transition_map = {
+            NonTerminal(state=source_state): Categorical(
+                {
+                    (
+                        NonTerminal(destination_state)
+                        if destination_state in non_terminals
+                        else Terminal(destination_state)
+                    ): prob
+                    for destination_state, prob in dist
+                }
+            )
+            for source_state, dist in transition_map.items()
+        }
+
+        self.non_terminal_states = list(self.transition_map.keys())
+
+    def __repr__(self) -> str:
+        display = ""
+
+        for source_state, dist in self.transition_map.items():
+            display += f"From State {source_state.state}:\n"
+            for dest_state, prob in dist:
+                opt = "Terminal " if isinstance(dest_state, Terminal) else ""
+                display += (
+                    f"\tTo {opt}State {dest_state.state} with Probability {prob:.3f}\n"
+                )
+
+        return display
+
+    def transition(self, state: NonTerminal[S]) -> FiniteDistribution[State[S]]:
+        return self.transition_map[state]
+
+    def compute_transition_matrix(self) -> np.ndarray:
+        num_states = len(self.non_terminal_states)
+        trans_matrix = np.zeros((num_states, num_states))
+
+        for row, source in enumerate(self.non_terminal_states):
+            for col, dest in enumerate(self.non_terminal_states):
+                trans_matrix[row, col] = self.transition(state=source).probability(
+                    outcome=dest
+                )
+
+        return trans_matrix
+
+    def compute_stationary_distribution(self) -> FiniteDistribution[S]:
+        eigen_vals, eigen_vecs = np.linalg.eig(self.compute_transition_matrix().T)
+        index_of_unit_eigenvalue = np.argmin(np.abs(eigen_vals - 1))
+        unit_eigenvector = eigen_vecs[:, index_of_unit_eigenvalue].real
+
+        return Categorical(
+            {
+                self.non_terminal_states[i].state: prob.item()
+                for i, prob in enumerate(unit_eigenvector)
+            }
+        )
